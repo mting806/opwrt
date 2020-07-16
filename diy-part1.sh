fddfc45b5ff62224fe96136876245f1762ad89b9
mting <mting806@msn.com>
2020-07-16 11:45:25 +0200

    up

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/mting806/opwrt/tree/master/packages' feeds.conf.default
